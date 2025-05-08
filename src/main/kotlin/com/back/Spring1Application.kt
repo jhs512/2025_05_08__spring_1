0f1269386e1b4e86e14adf63827826d882239ccb
jhs512 <jangka512@gmail.com>
2025-05-08 14:52:05 +0900

    work

---
+++ src/main/kotlin/com/back/Spring1Application.kt
@@ -0,0 +1,11 @@
+package com.back
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Spring1Application
+
+fun main(args: Array<String>) {
+    runApplication<Spring1Application>(*args)
+}
---
+++ src/test/kotlin/com/back/Spring1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.back
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Spring1ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
