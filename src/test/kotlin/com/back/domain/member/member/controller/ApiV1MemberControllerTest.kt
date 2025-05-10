75c452d5aa71e96f91cfaaa1ed8ec3d529b8cd48
jhs512 <jangka512@gmail.com>
2025-05-10 09:33:27 +0900

    work

--- src/test/kotlin/com/back/domain/member/member/controller/ApiV1MemberControllerTest.kt
+++ src/test/kotlin/com/back/domain/member/member/controller/ApiV1MemberControllerTest.kt
@@ -2,7 +2,7 @@ package com.back.domain.member.member.controller
 
 import com.back.domain.member.member.service.MemberService
 import com.back.standard.extensions.getOrThrow
-import org.hamcrest.Matchers
+import org.hamcrest.Matchers.startsWith
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -14,13 +14,13 @@ import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 import org.springframework.transaction.annotation.Transactional
 import java.nio.charset.StandardCharsets.UTF_8
 
 @SpringBootTest
 @ActiveProfiles("test")
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc // mockMvc 변수 주입 받을 수 있도록 세팅
 @Transactional // 테스트 환경에서의 @Transactional 는 @Transactional + @Rollback(true) 와 동일하다.
 class ApiV1MemberControllerTest {
     @Autowired
@@ -52,22 +52,22 @@ class ApiV1MemberControllerTest {
         val member = memberService.findByUsername("user1").getOrThrow()
 
         resultActions
-            .andExpect(MockMvcResultMatchers.handler().handlerType(ApiV1MemberController::class.java))
-            .andExpect(MockMvcResultMatchers.handler().methodName("login"))
-            .andExpect(MockMvcResultMatchers.status().isOk())
-            .andExpect(MockMvcResultMatchers.jsonPath("$.resultCode").value("200-1"))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.msg").value("${member.nickname}님 환영합니다."))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.data").exists())
-            .andExpect(MockMvcResultMatchers.jsonPath("$.data.id").value(member.id))
+            .andExpect(handler().handlerType(ApiV1MemberController::class.java))
+            .andExpect(handler().methodName("login"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.resultCode").value("200-1"))
+            .andExpect(jsonPath("$.msg").value("${member.nickname}님 환영합니다."))
+            .andExpect(jsonPath("$.data").exists())
+            .andExpect(jsonPath("$.data.id").value(member.id))
             .andExpect(
-                MockMvcResultMatchers.jsonPath("$.data.createDate")
-                    .value(Matchers.startsWith(member.createDate.toString().substring(0, 20)))
+                jsonPath("$.data.createDate")
+                    .value(startsWith(member.createDate.toString().substring(0, 20)))
             )
             .andExpect(
-                MockMvcResultMatchers.jsonPath("$.data.modifyDate")
-                    .value(Matchers.startsWith(member.modifyDate.toString().substring(0, 20)))
+                jsonPath("$.data.modifyDate")
+                    .value(startsWith(member.modifyDate.toString().substring(0, 20)))
             )
-            .andExpect(MockMvcResultMatchers.jsonPath("$.data.nickname").value(member.nickname))
+            .andExpect(jsonPath("$.data.nickname").value(member.nickname))
     }
 
     @Test
@@ -91,10 +91,10 @@ class ApiV1MemberControllerTest {
             .andDo(MockMvcResultHandlers.print())
 
         resultActions
-            .andExpect(MockMvcResultMatchers.handler().handlerType(ApiV1MemberController::class.java))
-            .andExpect(MockMvcResultMatchers.handler().methodName("login"))
-            .andExpect(MockMvcResultMatchers.status().isBadRequest())
-            .andExpect(MockMvcResultMatchers.jsonPath("$.resultCode").value("400-1"))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.msg").value("username-NotBlank-must not be blank"))
+            .andExpect(handler().handlerType(ApiV1MemberController::class.java))
+            .andExpect(handler().methodName("login"))
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.resultCode").value("400-1"))
+            .andExpect(jsonPath("$.msg").value("username-NotBlank-must not be blank"))
     }
 }
